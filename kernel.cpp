a56cc786894b2191afcbd96384f42342657271ac
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2024-01-25 04:16:33 -0600

    Update kernel.cpp to work without a linked library
    Should have done this earlier, probably.
    
    Honestly, the faux-printf function that I have here is not ideal for several reasons. There's less control over color, for instance. It's written a bit more haphazardly than the one on OSDev, for second.
    
    Also, in general, I'm not as big of a fan of C++. I have coded in C++ in the past, so I'm familiar with the language, but I'm not sure that I want to commit to it for this entire project.
    
    I'll come back and rewrite everything later if that's the case. For now, I'm staying aligned with WYOOS. I can edit everything after I've bundled it with LFS.

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,14 @@
+//This below function is terrible, maybe replace it with the one from the OSDev wiki, with modification.
+//I'm tempted to just do this in Assembly because it's simpler, to be honest, but that would negate the point of having this kernel here in a .cpp file
+//Honestly, I'm thinking of writing the kernel in another language, anyway, because I'm more of a fan of C and Rust, but we'll go with C++ for now as it's WYOOS's recommendation
+void printf(char* str) //accepts a a string of characters, this is a custom-made printf to avoid library linking since the library is not bundled with the kernel, but this creats a compilation error
+{
+  unsigned short* VideoMemory = (unsigned short*)0xb8000; //defines the address for VideoMemory, which is where the display array for the screen begins
+
+  for (int i=0; str[i] != '\0'; ++i) //creates a loop that ends at the null byte, essentially going through a string one character at a time, I think?
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; //sends the string to the screen, uses the high byte for white text on black background, although this seems like the wrong format?
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber, ) //Accepts the multiboot_structure from the Assembly file as an argument, and also the magicnumber for... some reason?
 {
   printf("Hewwo wowld owo");
