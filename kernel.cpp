2bbf37cca8115601d4765396a81b9092573b4a11
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2024-01-25 02:49:14 -0600

    Update kernel.cpp, minimum script written
    Will definitely come back to this.

--- kernel.cpp
+++ kernel.cpp
@@ -1,1 +1,6 @@
+void kernelMain()
+{
+  printf("Hewwo wowld owo");
 
+  while(1); //This puts the program into an infinite loop to prevent the kernel from stopping. This... seems terrible. I'll have to modify it later, maybe if/when I rewrite this in C.
+}
